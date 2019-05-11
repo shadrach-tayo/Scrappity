b110aabbd6f86ab70afcc8a2e907f17bc5a085e4
Shadrach Temitayo <shadrachtemitayo@gmail.com>
2019-05-11 02:14:25 +0100

    change cron job interval and return uniqeCount of data

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import './lib/cron';
+import { getInstagramCount, getTwitterCount } from './lib/scrapper';
+import db from './lib/db';
+import { uniqueCount } from './lib/utils';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get('/scrape', async (req, res, next) => {
   console.log('Scraping');
@@ -12,6 +17,15 @@ app.get('/scrape', async (req, res, next) => {
   ]);
 
   res.json({ iCount, tCount });
+});
+
+app.get('/data', async (req, res, next) => {
+  // get the scrape data
+  const { twitter, instagram } = db.value();
+  const uniqueTwitter = uniqueCount(twitter);
+  const uniqueInstagram = uniqueCount(instagram);
+  // respond with json
+  res.json({ twitter: [...uniqueTwitter], instagram: [...uniqueInstagram] });
 });
 
 app.listen(2900, () =>
